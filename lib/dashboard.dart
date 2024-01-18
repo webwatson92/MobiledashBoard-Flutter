9bce8db62f2bc7bc4f7551cc582642cca78176b8
HardFive <webwatson92@gmail.com>
2024-01-18 11:42:56 +0100

    Le design du Dashboard est terminé

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -2,45 +2,169 @@ import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
   var height, width; 
-  
+
+  List imgData = [
+    "images/mcqs.png",
+    "images/quiz.png",
+    "images/pastpapers.png",
+    "images/pdf.jpeg",
+    "images/job.png",
+    "images/about.jpeg",
+    "images/woman.png",
+  ];
+
+  List titles = [
+    "MCQS",
+    "QUIZ",
+    "Past Papers",
+    "PDF",
+    "Job",
+    "About",
+  ];
+
   @override
   Widget build(BuildContext context) {
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Container(
-        color: Colors.indigo,
-        height: height,
-        width: width,
-        child: Column(
-          children:[
-            Container(
-              decoration: BoxDecoration(),
-              height: height * 0.25,
-              width: width,
-              child: Column(
-                children: [
-                  Row(children: [
-                    InkWell(
-                      onTap: (){},
-                      child: Icon(Icons.sort, color: Colors.white),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          color: Colors.indigo,
+          height: height,
+          width: width,
+          child: Column(
+            children:[
+              Container(
+                decoration: BoxDecoration(),
+                height: height * 0.25,
+                width: width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(
+                        top: 35, 
+                        left: 20,
+                        right: 20
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                        InkWell(
+                          onTap: (){},
+                          child: Icon(
+                            Icons.sort, 
+                            color: Colors.white,
+                            size: 40,
+                          ),
+                        ),
+                        Container(
+                          height: 50,
+                          width: 50,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: Colors.white,
+                            image: DecorationImage(
+                              image: AssetImage(
+                                 "images/woman.png",),
+                            ),
+                          ),
+                        ),
+                      ],),
                     ),
-                  ],),
-                ],
+                    const Padding(
+                      padding: EdgeInsets.only(
+                        top: 20, 
+                        left: 30,
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Dashboard",
+                            style: TextStyle(
+                              fontSize: 30,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                              letterSpacing: 1
+                            ),
+                          ),
+                          SizedBox(height: 10,),
+                          Text(
+                            "Last Update: 7 Jan 2024",
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white54,
+                              letterSpacing: 1
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Container(
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  topLeft: Radius.circular(50),
-                  topRight: Radius.circular(50)
-                )
+              SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Container(
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(50),
+                      topRight: Radius.circular(50)
+                    )
+                  ),
+                  height: height * 0.75,
+                  width: width,
+                  // padding: EdgeInsets.only(bottom: 20),
+                  child: GridView.builder(
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 1.1,
+                      mainAxisSpacing: 25
+                      ),
+                      shrinkWrap: true,
+                      physics: NeverScrollableScrollPhysics(),
+                      itemCount: imgData.length,
+                      itemBuilder: (context, index){
+                        return InkWell(
+                          onTap: (){},
+                          child: Container(
+                            margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(20),
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black26,
+                                  spreadRadius: 1,
+                                  blurRadius: 6,
+                                ),
+                              ],
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                Image.asset(imgData[index],
+                                  width: 100, 
+                                ),
+                                Text(titles[index],
+                                style: TextStyle(
+              
+                                ),
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      
+                  ),
+                ),
               ),
-              height: height * 0.75,
-              width: width,
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
